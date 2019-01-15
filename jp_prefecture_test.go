188eb07f6cfbfde9a2027be03b3f197f14e3ef59
k-motoyan <kenta.motoyanagi@diverse-inc.com>
2019-01-15 17:32:45 +0900

    Added text for FindByKanji.

--- jp_prefecture_test.go
+++ jp_prefecture_test.go
@@ -7,7 +7,7 @@ import (
 func TestFindByCode(t *testing.T) {
 	var (
 		prefecture Prefecture
-		ok bool
+		ok         bool
 	)
 
 	prefecture, ok = FindByCode(1)
@@ -19,6 +19,30 @@ func TestFindByCode(t *testing.T) {
 	}
 
 	prefecture, ok = FindByCode(0)
+	if ok {
+		t.Fatal()
+	}
+	if prefecture != nil {
+		t.Fatal()
+	}
+}
+
+func TestFindByKanji(t *testing.T) {
+	findValues := []string{
+		"北海道", "東京", "東京都", "大阪", "大阪府", "京都", "京都府", "沖縄", "沖縄県",
+	}
+
+	for _, value := range findValues {
+		prefecture, ok := FindByKanji(value)
+		if !ok {
+			t.Fatal()
+		}
+		if prefecture == nil {
+			t.Fatal()
+		}
+	}
+
+	prefecture, ok := FindByKanji("北海")
 	if ok {
 		t.Fatal()
 	}
